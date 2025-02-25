0a377fca3d0bee3df0e87d54d0661841ae9b877b
Jaciecherron <jaciecherron@gmail.com>
2025-02-24 17:19:55 -0700

    Added netlify link

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ export default function App() {
       Weather App
       <footer>
         This project was codded by{" "}
-        <a href="https://github.com/Jaciecherron" target="_blank" rel="noreferrer">Jacie Spivey </a> and is
-        hosted on Netlify
+        <a
+          href="https://github.com/Jaciecherron"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jacie Spivey{" "}
+        </a>{" "}
+        and is hosted on{" "}
+        <a
+          href="https://dainty-granita-d17123.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );

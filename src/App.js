66a3541a2efd845d303c9bc02cadd48087b58e45
Jaciecherron <jaciecherron@gmail.com>
2025-02-24 17:29:27 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was codded by{" "}
-        <a
-          href="https://github.com/Jaciecherron"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Jacie Spivey{" "}
-        </a>{" "}
-        and is hosted on{" "}
-        <a
-          href="https://dainty-granita-d17123.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was codded by{" "}
+          <a
+            href="https://github.com/Jaciecherron"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jacie Spivey{" "}
+          </a>{" "}
+          and is hosted on{" "}
+          <a
+            href="https://dainty-granita-d17123.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

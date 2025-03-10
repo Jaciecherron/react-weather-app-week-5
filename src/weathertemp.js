d93f75b4944655809c3112bdd68a97398735e621
Jaciecherron <jaciecherron@gmail.com>
2025-03-10 12:22:51 -0600

    Removed farenheit component

--- src/weathertemp.js
+++ src/weathertemp.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemp (props) {
-const [unit, setUnit] = useState ("Celsius");
-
-function convertToCelsius (event) {
-  event.preventDefault();
-  setUnit("celsius");
-}
-
-function convertToFarenheit (event) {
-  event.preventDefault();
-  setUnit("farenheit");
-}
-
-function farenheit () {
-  return (props.celsius * 9) /5 +32;
-}
-
-if (unit === "celsius") {
-
   return (
     <div className="temp">
       <span className="temperature">{Math.round(props.celsius)}</span>{" "}
-      <span className="unit">
-        °C /
-        <a href="/" onClick={convertToFarenheit}>
-          °F
-        </a>{" "}
-      </span>
+     <span className="unit">°C </span>
     </div>
-  ); }
-  else {
-    return (
-<div className="temp">
-  <span className="temperature">{Math.round(farenheit())}</span>{" "}
-  <span className="unit">
-    <a href="/" onClick={convertToCelsius}>
-      °C
-    </a>{" "}
-    /°F
-  </span>
-</div>); 
-  } }
+  );
+} 
